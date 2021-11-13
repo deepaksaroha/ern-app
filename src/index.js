8d871dde9ce14fd6ca444b118b837ef5fe3b1e6a
deepaksaroha <deepaksaroha1993@gmail.com>
2021-11-13 21:55:09 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function App() {
             {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
             <Switch>
-                {/* <Route exact path="/developers/:developerId" component={DeveloperProfile} /> */}
+                <Route exact path="/developers/:developerId" component={DeveloperProfile} />
                 <Route exact path="/" component={HomePage} />
             </Switch>
         </Router>
